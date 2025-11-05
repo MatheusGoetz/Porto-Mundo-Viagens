b413e87861cedf5226ee157519bf9d948f71f484
Matheus Goetz <goetz.dev@gmail.com>
2025-11-04 22:25:12 -0300

    fixed: correção no arquivo vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { createServer } from "./server";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -13,13 +12,6 @@ export default defineConfig(({ mode }) => ({
       deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
     },
   },
-  build: {
-  outDir: 'dist',
-  rollupOptions: {
-    input: 'server/index.ts', // ou seu entry correto
-  }
-}
-,
   plugins: [react(), expressPlugin()],
   resolve: {
     alias: {
@@ -32,11 +24,9 @@ export default defineConfig(({ mode }) => ({
 function expressPlugin(): Plugin {
   return {
     name: "express-plugin",
-    apply: "serve", // Only apply during development (serve mode)
+    apply: "serve",
     configureServer(server) {
       const app = createServer();
-
-      // Add Express app as middleware to Vite dev server
       server.middlewares.use(app);
     },
   };
